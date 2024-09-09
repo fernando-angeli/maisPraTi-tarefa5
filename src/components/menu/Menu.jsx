aaef47e6bbc772ba20f31f58cd31deae464bb5b3
fernando-angeli <fernando_angeli@hotmail.com>
2024-09-09 15:57:36 -0300

    :sparkles: feat: bootstrap responsive menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,90 +1,53 @@
-import styled from "styled-components";
 import Title from "../title/Title";
 import Button from "../button/Button";
-import { Link } from "react-router-dom";
 import { useAuth } from "../../provider/authProvider";
 import LogoutIcon from "@mui/icons-material/Logout";
-
-const MenuContainer = styled.nav`
-  width: 100%;
-  height: 5rem;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  border-bottom: 1px solid;
-  margin-bottom: 2rem;
-  font-family: Impact, sans-serif;
-`;
-
-const ItensMenu = styled.ul`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  list-style: none;
-`;
-
-const Item = styled.li`
-  margin-left: 2rem;
-`;
-
-const StyledLinkTitle = styled(Link)`
-  text-decoration: none;
-  list-style: none;
-  font-size: 2rem;
-  color: #155ec4;
-`;
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  list-style: none;
-  font-size: 1.2rem;
-  color: black;
-  &:hover {
-    color: #155ec4;
-    border-top: 1px solid blue;
-    padding-top: 0.5rem;
-    transition: 100ms ease-out;
-  }
-`;
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Menu() {
   const { loggedUser, logout } = useAuth();
   return (
-    <MenuContainer>
-      <StyledLinkTitle to="/">
-        <Title title="MultiApp" />
-      </StyledLinkTitle>
+    <>
       {loggedUser && (
-        <ItensMenu>
-          <Item>
-            <StyledLink to="/translator">Translator</StyledLink>
-          </Item>
-          <Item>
-            <StyledLink to="/moviesSearch">Movies Search</StyledLink>
-          </Item>
-          <Item>
-            <StyledLink to="/qrCodeGenerator">QR Code Generator</StyledLink>
-          </Item>
-          <Item>
-            <StyledLink to="/searchIp">Search IP</StyledLink>
-          </Item>
-          <Item>
-            <StyledLink to="/toDoList">ToDo List</StyledLink>
-          </Item>
-          <Item>
-            <Button
-              description={loggedUser.name}
-              border="1px solid"
-              borderRadius="0.5rem"
-              backgroundColor="transparent"
-              height="2.5rem"
-              fontSize="1rem"
-              onClick={() => logout()}
-            >
-              <LogoutIcon />
-            </Button>
-          </Item>
-        </ItensMenu>
+        <Navbar
+          collapseOnSelect
+          sticky="top"
+          expand="lg"
+          className="w-100 mb-2"
+          style={{ backgroundColor: "#ECECF0", borderRadius: "0.5rem" }}
+        >
+          <Container>
+            <Navbar.Brand href="/" className="px-3">
+              <Title title="MultiApp" margin="0.5rem 0rem" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="w-100 d-flex justify-content-evenly">
+                <Nav.Link href="/translator">Translator</Nav.Link>
+                <Nav.Link href="/moviesSearch">Movies</Nav.Link>
+                <Nav.Link href="/qrCodeGenerator">QR Code</Nav.Link>
+                <Nav.Link href="/searchIp">Search IP</Nav.Link>
+                <Nav.Link href="/toDoList">ToDo List</Nav.Link>
+                <Button
+                  description={loggedUser.name}
+                  border="1px solid"
+                  borderRadius="0.5rem"
+                  backgroundColor="transparent"
+                  height="2.5rem"
+                  fontSize="1rem"
+                  onClick={() => logout()}
+                  margin="0"
+                >
+                  <LogoutIcon />
+                </Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       )}
-    </MenuContainer>
+    </>
   );
 }
