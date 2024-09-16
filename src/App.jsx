f6f554c7b96c90f7b2cb72b4d522ed69a9100cac
fernando-angeli <fernando_angeli@hotmail.com>
2024-09-15 21:47:55 -0300

    refactor import AuthProvider on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
-//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-//import Login from "./components/login/Login";
-//import Register from "./components/register/Register";
-//import MovieSearchEngine from "./components/movieSearchEngine/MovieSearchEngine";
-//import QRCodeGenerator from "./components/qr-code/QrCodeGenerator";
-//import SearchIp from "./components/searchIp/SearchIp";
-//import Translator from "./components/translator/Translator";
-//import ToDoList from "./components/toDoList/ToDoList";
 import styled from "styled-components";
-import AuthProvider from "./provider/AuthProvider";
 import Routes from "./routes";
+import AuthProvider from "./provider/AuthProvider";
 
 const AppContainer = styled.div`
   background-color: #ececf0;
